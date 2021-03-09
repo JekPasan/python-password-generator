0b3cfbda2993399b027dfb254e69636668582e7a
JekPasan <68905668+JekPasan@users.noreply.github.com>
2021-03-09 13:45:37 +0200

    Better password generation and cleaner code

--- PassGenerator.py
+++ PassGenerator.py
@@ -1,20 +1,29 @@
 from random import randint
 class PassGenerator(object):
-  def __init__(self) -> None:
-      pass
+  def add(self, x: str, y: str) -> None:
+    # x = pasword
+    # y = what you want to pick to add to x
+    random = y[randint(0, len(y)-1)]
+    x += random
+    return x
+  
   def generate(self) -> None:
     raw_parameters = input("words you want within your password: ")
     parameters = raw_parameters.split(",")
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    lower_alphabet = "abcdefghijklmnopqrstuvwxyz"
+    upper_alphabet = "ABCDEFGHIJKLMNOPQRTUVWXYZ"
+    signs = "!@#$%^&*-_=+"
     digits = "0123456789"
     password = ""
     
     while len(parameters) != 0:
-      # decides whether to add a chosen paramter, a random letter or a digit to the currently generated password
-      a = randint(1, 4+1)
-      # a <= 2 --> add a parameter
-      # a = 3 --> add a letter
-      # a = 4 --> add a digit
+      # decides whether to add a chosen paramter, a random letter, digit or a sign to the currently generated password
+      a = randint(1, 5+1)
+      # a = 1 --> add a parameter
+      # a = 2 --> add a lower case letter
+      # a = 3 --> add a upper case letter
+      # a = 4 --> add a sign
+      # a = 5 --> add a digit
 
       if len(password) >= 10 and len(parameters) <= len(parameters)/2:
         # add the remainign parameters if the password is too long
@@ -24,14 +33,16 @@ class PassGenerator(object):
           # the else gets executed after the for loop finished
           break
       
-      if a <= 2:
+      if a == 1:
         random_parametrer = parameters[randint(0, len(parameters)-1)]
         password += random_parametrer
         parameters.remove(random_parametrer)
+      elif a == 2:
+        password = self.add(password, lower_alphabet)
       elif a == 3:
-        random_letter = alphabet[randint(0, len(alphabet))]
-        password += random_letter
+        password = self.add(password, upper_alphabet)
       elif a == 4:
-        random_digit = digits[randint(0, len(digits))]
-        password += random_digit
+        password = self.add(password, signs)
+      elif a == 5:
+        password = self.add(password, digits)
     print("generated password is: " + password)
